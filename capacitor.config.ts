0a377916693d7fd37a8795390d73a63fe4d1fd79
Takashi_ISHIDA <tks_ishida25@mac.lan>
2026-06-18 21:36:40 +0900

    92 drugs stable version

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'jp.ishida.egfr',
+  appName: 'eGFR薬剤検索',
+  webDir: 'dist'
+};
+
+export default config;
